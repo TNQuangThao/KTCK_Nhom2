fba706b33fc2422dd6ee38430e85b601b82be60e
TNQuangThao <thaotruong23082004@gmail.com>
2024-12-20 00:57:10 +0700

    Thao Update

--- app/src/main/java/com/example/ktcuoiki/APIService.java
+++ app/src/main/java/com/example/ktcuoiki/APIService.java
@@ -5,6 +5,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface APIService {
+    //Lấy sản phẩm từ database
     @GET("getSanPham.php")
     Call<List<ModelSanPham>> getSanPham();
 }
